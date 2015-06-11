6857a06a5479a6546de56867707fda115c699702
amitabhs <amitab11@gmail.com>
2015-06-11 17:04:06 +0530

    iloi;u

--- src/com/amitabh/listenmy/MainActivity.java
+++ src/com/amitabh/listenmy/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends Activity implements OnClickListener,
 	private void setAllListener() {
 		int i = 0;//klj
 		
-		//new comment by jalgaskldgklasdlgjalgjaklsjg
+		//new comment by Ho HO HO
 		next.setOnClickListener(this);
 		previous.setOnClickListener(this);
 		playPause.setOnCheckedChangeListener(this);
